cdce6a97570866f4ec5b4d29e9cc92a19565bfa3
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2024-12-10 20:50:45 +0530

    Basic setup of layout of the website

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { BuyCredit, Home, Result } from './pages/page_index'
+
 
 function App() {
 
   return (
-    <>
-        
-    </>
+    <div className='min-h-screen px-4 sm:px-10 md:px-14 lg:px-28 bg-gradient-to-b from-teal-50 to-orange-50'>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/result" element={<Result />} />
+      <Route path="/buy" element={<BuyCredit />} />
+    </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/page_index.js
@@ -0,0 +1,9 @@
+import Home from "../pages/Home/Home"
+import BuyCredit from "./BuyCredit/BuyCredit"
+import Result from "./Result/Result"
+
+export {
+    Home,
+    BuyCredit,
+    Result,
+}
