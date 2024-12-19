3ed49b394b93088fdc3ec6d47d7c4d8d8f783aac
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2024-12-19 16:42:07 +0530

    Setup mongodb for the database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express"
 import cors from "cors"
 import "dotenv/config"
+import connectDB from "./config/mongodb.js"
 
 const port = process.env.PORT || 4000
 const app = express()
+
+await connectDB()
 
 app.use(express.json())
 app.use(cors())
